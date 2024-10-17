4ea00fbbe0e972dcd8d1f6bd1ae85526a3959b92
rohat güneş <rohatgns44@gmail.com>
2024-10-17 15:41:28 +1100

    Add project files.

---
+++ IdentityandDataProtection/Controllers/UserController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.DataProtection;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    private readonly IDataProtector _protector;
+
+    public UserController(AppDbContext context, IDataProtectionProvider provider)
+    {
+        _context = context;
+        _protector = provider.CreateProtector("PasswordProtector");
+    }
+
+    // GET: api/user
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return Ok(users);
+    }
+
+    // POST: api/user
+    [HttpPost]
+    public async Task<IActionResult> AddUser([FromBody] User user)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        // Encrypt the password
+        user.Password = _protector.Protect(user.Password);
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return Ok(user);
+    }
+
+    // GET: api/user/{id}
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        if (user == null)
+            return NotFound();
+
+        return Ok(user);
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateUser([FromBody] User user)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        // Şifreyi burada şifrelemeniz gerekebilir
+        user.Password = HashPassword(user.Password);
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return Ok(user);
+    }
+
+    private string HashPassword(string password)
+    {
+        // Şifreleme işlemi için bir method ekleyin. 
+        // Örneğin, ASP.NET Core Identity kullanıyorsanız, PasswordHasher kullanabilirsiniz.
+        return password;
+    }
+}
---
+++ IdentityandDataProtection/DbContext/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+public void ConfigureServices(IServiceCollection services)
+{
+    services.AddControllers();
+    services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+    // Data Protection setup
+    services.AddDataProtection();
+}
